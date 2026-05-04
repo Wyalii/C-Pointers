0e12fd1ab0d8704ab4762dfc61dcdda73d31c3c7
Wyalii <gigaurigiorgi2727@gmail.com>
2026-05-04 21:46:10 +0400

    alr dereferencing easy.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main()
     int a = 5;
     int *p;
     p = &a;
+    *p = 10; // dereferencing!!!!!
     printf("print of a variable: %d\n", a);
     printf("print of a p variable: %d\n", p);
     printf("print of *p: %d\n", *p);
