1ef39a696ed59d9c687c65247cd8a8c143829959
Wyalii <gigaurigiorgi2727@gmail.com>
2026-05-05 21:35:01 +0400

    small pointer arithmetic i guess.

--- main.c
+++ main.c
@@ -5,9 +5,14 @@ int main()
     int *p;
     p = &a;
     *p = 10; // dereferencing!!!!!
+    int b = 20;
+    *p = b;
     printf("print of a variable: %d\n", a);
     printf("print of a p variable: %d\n", p);
     printf("print of *p: %d\n", *p);
     printf("print of &a: %d\n", &a);
+    printf("printed p+1: %d\n", p + 1);
+    printf("size of integer is %d bytes\n", sizeof(int));
+    printf("value at P+1 is: %d", *p + 1);
     return 0;
 }
